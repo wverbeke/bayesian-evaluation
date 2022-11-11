7aa47bafc294cc8848df09b72e1e4a8af14b2404
Willem Verbeke <willem_verbeke@hotmail.com>
2022-11-12 00:43:27 +0100

    Fix missing classes in pytorch interface for CIFAR

--- load_datasets.py
+++ load_datasets.py
@@ -79,6 +79,12 @@ class CIFAR10Loader(Dataset):
             _SHARED_TRANSFORMS
         ])
 
+    # Torchvision data sets have inconsistent interfaces.
+    # The CIFAR10 and CIFAR100 data sets do not have the 'classes' attribute.
+    @classmethod
+    def classes(cls):
+        return ["plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+
 
 
 class CIFAR100Loader(Dataset):
@@ -93,6 +99,29 @@ class CIFAR100Loader(Dataset):
             transforms.RandomHorizontalFlip(),
             _SHARED_TRANSFORMS
         ])
+
+    @classmethod 
+    def classes(cls):
+        return ["beaver", "dolphin", "otter", "seal", "whale",
+                "aquarium fish", "flatfish", "ray", "shark", "trout",
+                "orchids", "poppies", "roses", "sunflowers", "tulips",
+                "bottles", "bowls", "cans", "cups", "plates",
+                "apples", "mushrooms", "oranges", "pears", "sweet peppers",
+                "clock", "computer keyboard", "lamp", "telephone", "television",
+                "bed", "chair", "couch", "table", "wardrobe",
+                "bee", "beetle", "butterfly", "caterpillar", "cockroach",
+                "bear", "leopard", "lion", "tiger", "wolf",
+                "bridge", "castle", "house", "road", "skyscraper",
+                "cloud", "forest", "mountain", "plain", "sea",
+                "camel", "cattle", "chimpanzee", "elephant", "kangaroo",
+                "fox", "porcupine", "possum", "raccoon", "skunk",
+                "crab", "lobster", "snail", "spider", "worm",
+                "baby", "boy", "girl", "man", "woman",
+                "crocodile", "dinosaur", "lizard", "snake", "turtle",
+                "hamster", "mouse", "rabbit", "shrew", "squirrel",
+                "maple", "oak", "palm", "pine", "willow"
+                "bicycle", "bus", "motorcycle", "pickup truck", "train",
+                "lawn-mower", "rocket", "streetcar", "tank", "tractor"]
 
 
 
