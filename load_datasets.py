dcd4eb483976000fa51bbbc000e569ef186f863a
Willem Verbeke <willem_verbeke@hotmail.com>
2022-11-16 22:54:52 +0100

    Do not drop remainder during eval

--- load_datasets.py
+++ load_datasets.py
@@ -14,7 +14,7 @@ _DATALOADER_KWARGS = {"num_workers": os.cpu_count(), "prefetch_factor": 4}
 
 def _build_data_loader(dataset: Callable, train: bool, transforms: Callable, batch_size: int):
     data = dataset(train=train, transform=transforms, **_DATASET_KWARGS)
-    return DataLoader(data, batch_size=batch_size, shuffle=train, drop_last=True, **_DATALOADER_KWARGS)
+    return DataLoader(data, batch_size=batch_size, shuffle=train, drop_last=train, **_DATALOADER_KWARGS)
 
 
 class Dataset:
