41911ec5870ea5cc793a910f779814df70477031
Willem Verbeke <willem_verbeke@hotmail.com>
2022-11-17 21:40:49 +0100

    Transpose the confusion matrix during its initial computation to adhere to the standard axis alignment

--- evaluate_neural_networks.py
+++ evaluate_neural_networks.py
@@ -8,6 +8,11 @@ import numpy as np
 from data_tasks import DataTask, TASK_REGISTER, DEVICE, DEVICE_CPU, EVAL_DIRECTORY
 
 def compute_confusion_matrix(data_task: DataTask):
+    """Compute the confusion matrix on the evaluation data for a given task and its model.
+
+    The confusion matrix will be returned in the usual format where true labels are columns and
+    predictions are rows.
+    """
 
     # Load the model.
     model = data_task.load_model()
@@ -31,13 +36,17 @@ def compute_confusion_matrix(data_task: DataTask):
     label_tensor = torch.cat(labels, dim=0).cpu()
     cm_computer = MulticlassConfusionMatrix(num_classes=data_task.num_classes()).to(DEVICE_CPU)
     cm = cm_computer(prediction_tensor, label_tensor)
+
+    # Pytorch uses the opposite convention from what is usual, i.e. the predictions are columns and 
+    # the true labels rows. We want it in the usual format where the true labels are columns.
+    cm = np.transpose(cm)
     return cm
 
 
 
 if __name__ == "__main__":
     os.makedirs(EVAL_DIRECTORY, exist_ok=True)
-    for task in TASK_REGISTER:
+    for task in TASK_REGISTER[:1]:
         print(f"Computing confusion matrix for {task.name()} task.")
         cm = compute_confusion_matrix(task)
         np.save(task.confusion_matrix_path(), cm.numpy())
